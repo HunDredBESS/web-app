f9d1d406f1a352f99c97c94ff23b900a9c2d9450
HunDredBESS <roikao.nine@gmail.com>
2023-06-30 02:51:19 +0700

    edit

--- src/app/TimeSeries/page.tsx
+++ src/app/TimeSeries/page.tsx
@@ -9,6 +9,9 @@ export default function page({}: Props) {
    <div className='w-full h-20 bg-slate-50 text-zinc-950 text-5xl align-middle m-1 p-1 items-center'>
     <h1>Time Series</h1>
    </div>
+   <div className='w-full h-20 bg-blue-500 text-zinc-950 text-5xl align-middle m-1 p-1 items-center'>
+    <h1>Time Series</h1>
+   </div>
     </>
   )
 }
